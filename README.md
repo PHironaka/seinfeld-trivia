58b56fac3857197f3f8d0fdd0064be637bff212a
Peter <peterhironaka@mac.com>
2017-04-03 01:14:59 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Seinfeld Trivia!
+
+## Premise
+
+This game is design to test a user's knowledge of Seinfeld.
+
+## Requirements
+- Needs to be two player
+- Must be fun!
+
+## User Story
+- Once the application is loaded, the player will have the option to 
+
+## Toolkit
+- Block Element Modifier (BEM) Methodology for css naming conveniton. 
+
+## Naming Convention
+
+
+## Learnings 
---
+++ js/app.js
@@ -0,0 +1,117 @@
+//variable holding all the games questions, player 
+var game = {
+  player1: {score: 0},
+  player2: {score: 0},
+  questions: [
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+
+    },
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    },
+
+    {
+      image: '',
+      question: "What is the name of blah blah blah",
+      answers: [
+        {value: "George", correct: false},
+        {value: "George", correct: false},
+        {value: "George", correct: true},
+        {value: "George", correct: false},
+      ]
+    }
+
+  ]
+}
